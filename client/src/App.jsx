44b3b7a5dfeae42ddf4279d99bd06603cf700da3
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-09-17 08:55:32 +0530

    Background image setting changed as it was not displyed after deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,21 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom"
 import HomePage from './pages/HomePage'
 import ChatPage from './pages/ChatPage'
+import backgroundImage from "../public/bg.jpeg"
 
 
+const backgroundStyle = {
+  backgroundImage: `url(${backgroundImage})`, // Set background image
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  minHeight: "100vh",
+  display: "flex",
 
+};
 const App = () => {
   return (
-    <div className='App'>
+    <div className='App' style={backgroundStyle}>
 
       <Routes>
         <Route path='/' element={<HomePage />} />
