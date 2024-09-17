f08d0385090b4a64e4ccfb0e5c1e2508f3f34ef5
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-09-17 14:32:38 +0530

    Deployed origin added

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const app = express();
 const PORT = process.env.PORT || 8001
 const DB = process.env.DB_URL
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://chatapplication-mernstack.netlify.app/'
+}));
 app.use(express.json());
 
 app.use("/user", userRoutes)
